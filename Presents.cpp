6887ab761d1839bfc0d82ce9054397ef540a105f
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-03 21:37:41 +0530

    Update Presents.cpp

--- Presents.cpp
+++ Presents.cpp
@@ -1,1 +1,17 @@
 //	136A - Presents
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int n, r;
+ 
+int main(){
+	cin >> n;
+	int a[n];
+	for(int i=0; i<n; i++){
+		cin >> r;
+		a[r-1]=i+1;
+	}
+	for(int i=0; i<n; i++){
+		cout << a[i] << " ";
+	}
+}
