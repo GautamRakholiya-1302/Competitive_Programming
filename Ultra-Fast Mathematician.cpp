66abdf1994e2e806ef32e0aafa8691af666b2284
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-04 18:33:48 +0530

    Update Ultra-Fast Mathematician.cpp

--- Ultra-Fast Mathematician.cpp
+++ Ultra-Fast Mathematician.cpp
@@ -1,1 +1,13 @@
+61A - Ultra-Fast Mathematician
 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string c,d;
+	cin>>c>>d;
+	int kd=c.size();
+	for(int a=0;a<kd;a++)
+		cout<<(c[a]!=d[a]);
+ 
+}
