9c87f00f0f1e020a3372a56efbb5ff4057ce9d5a
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-07 23:00:08 +0530

    Update Is your horseshoe on the other hoof?.cpp

--- Is your horseshoe on the other hoof?.cpp
+++ Is your horseshoe on the other hoof?.cpp
@@ -1,1 +1,12 @@
-228A - Is your horseshoe on the other hoof?
+//228A - Is your horseshoe on the other hoof?
+#include <bits/stdc++.h>
+using namespace std;
+main(){
+	int x, n=4;
+	set <int> st;
+	while(n--){
+		cin>>x;
+		st.insert(x);
+	}
+	cout<<4-st.size();
+}
