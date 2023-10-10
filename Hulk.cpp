50d197d614d2ac1204d2f4bbc505c5d4dce3827a
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-10 19:39:51 +0530

    Create Hulk.cpp

---
+++ Hulk.cpp
@@ -0,0 +1,15 @@
+//705A - Hulk
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+cout<<"I hate";
+for(int i=1;i<n;++i){
+	cout<<(i%2==1?" that I love":" that I hate");
+}
+cout<<" it";
+return 0;
+}
