5b9710ee25fe782370e04722e77071cc9c215ebb
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-14 22:57:26 +0530

    Update Police Recruits.cpp

--- Police Recruits.cpp
+++ Police Recruits.cpp
@@ -1,1 +1,18 @@
+// 427A - Police Recruits
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n,b=0,c=0,a;
+	cin>>n;
+	for(int i=1;i<=n;i++)	{
+		cin>>a;
+		if(a<0&&b==0)
+		c++;
+		if(a>0)
+		b=b+a;
+		if(a<0&&b>0)
+		b=b+a;
+	}
+	cout<<c;
+}
 
