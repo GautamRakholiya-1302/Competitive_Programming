945e5f3b563f76341a0f927645668cc69c5caed9
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-16 23:09:04 +0530

    Update Anton and Polyhedrons.cpp

--- Anton and Polyhedrons.cpp
+++ Anton and Polyhedrons.cpp
@@ -1,1 +1,21 @@
 
+// 785A - Anton and Polyhedrons
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+int n,k=0;
+cin>>n;
+string s;
+while(n--){
+        cin>>s;
+    if(s=="Tetrahedron")k+=4;
+    if(s=="Cube")k+=6;
+    if(s=="Octahedron")k+=8;
+    if(s=="Dodecahedron")k+=12;
+    if(s=="Icosahedron")k+=20;
+}
+cout<<k;
+}
