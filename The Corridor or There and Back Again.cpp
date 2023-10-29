6cd6aa3056a9c6ad66f9685b84bd123613800fd0
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-29 23:53:56 +0530

    Update The Corridor or There and Back Again.cpp

--- The Corridor or There and Back Again.cpp
+++ The Corridor or There and Back Again.cpp
@@ -1,1 +1,25 @@
 // 1872B - The Corridor or There and Back Again
+#include <iostream>
+using namespace std;
+ 
+const int N = 103;
+ 
+void solve() {
+	int n, ans = 1e9;
+	cin >> n;
+	while (n--) {
+		int d, s;
+		cin >> d >> s;
+		ans = min(ans, d + (s - 1) / 2);
+	}
+	cout << ans << endl;
+}
+ 
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
