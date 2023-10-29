5e1e373e1851b03847285ba028c837b27e4ccb3c
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-29 23:55:46 +0530

    Update Chips on the Board.cpp

--- Chips on the Board.cpp
+++ Chips on the Board.cpp
@@ -1,1 +1,25 @@
 1879B - Chips on the Board
+
+
+#include <iostream>
+#include <algorithm>
+ 
+using namespace std;
+ 
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		long long n, a, b, asum = 0, bsum = 0, amin = INT_MAX, bmin = INT_MAX;
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			cin >> a;
+			asum += a, amin = min(amin, a);
+        }
+		for (int i = 0; i < n; i++) {
+			cin >> b;
+			bsum += b, bmin = min(bmin, b);
+		}
+		cout << min(asum + bmin * n, bsum + amin * n) << '\n';
+	}
+}
