deff0613ebe9d76038f464811f06da67dd3e4708
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-30 23:18:34 +0530

    Update Do Not Be Distracted!.cpp

--- Do Not Be Distracted!.cpp
+++ Do Not Be Distracted!.cpp
@@ -1,1 +1,31 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,i,k;
+		cin>>n;
+		string s;
+		cin>>s;
+		int a[26]={0};
+		for(i=0;i<n;i++)
+		{
+			k=s[i]-65;
+			a[k]++;
+			if(a[k]>=2)
+			{
+				if(s[i]!=s[i-1])
+				break;
+			}
+		}
+		if(i<n)
+		cout<<"NO";
+		else
+		cout<<"YES";
+		cout<<"\n";
+	}
+}
