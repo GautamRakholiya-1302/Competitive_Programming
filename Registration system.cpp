8182b6dc7b397883204534deb7c9e01924fd7b06
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-11-04 23:02:55 +0530

    Update Registration system.cpp

--- Registration system.cpp
+++ Registration system.cpp
@@ -1,1 +1,18 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+int t;
+string s;
+map < string,int > m;
+main()
+{
+    cin>>t;
+while(t--)
+{cin>>s;
+if(m[s]++)cout<<s<<m[s]-1;
+else
+ cout<<"OK";
+ cout<<endl;
+}
+    return 0;
+}
