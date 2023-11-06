bed7bd7dd420e27c899291af50b01f8ca8ca79dd
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-11-06 21:03:33 +0530

    Update Remove Smallest.cpp

--- Remove Smallest.cpp
+++ Remove Smallest.cpp
@@ -1,1 +1,25 @@
 // 1399A - Remove Smallest
+
+#include<bits/stdc++.h>
+using namespace std;
+int a[60];
+int main(){
+	int t,n;
+	cin>>t;
+	while(t--){
+		int flag=1;
+		cin>>n;
+		for(int i=0;i<n;i++){
+			cin>>a[i];
+		}
+		sort(a,a+n);
+		for(int i=1;i<n;i++){
+			if(a[i]-a[i-1]>1){
+				flag=0;
+				break;
+			}
+		}
+		if(flag)cout<<"YES\n";
+		else cout<<"NO\n";
+	}
+}
