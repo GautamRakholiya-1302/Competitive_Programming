3ee4e9ee9caf4adb4640be13bb80bb690bc3a8c9
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-11-06 21:06:10 +0530

    Update Yet Another Two Integers Problem.cpp

--- Yet Another Two Integers Problem.cpp
+++ Yet Another Two Integers Problem.cpp
@@ -1,1 +1,15 @@
 //1409A - Yet Another Two Integers Problem
+
+
+#include<iostream>
+using namespace std;
+int main(){
+	int a, b,t;
+	cin>>t;
+	while(t--){
+		cin>>a>>b;
+		
+ 
+		cout<<(abs(a-b)+9)/10<<endl;
+	}
+}
