d203c427335d557a76152da770f00dae3359a581
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-11-07 23:40:48 +0530

    Update Sum.cpp

--- Sum.cpp
+++ Sum.cpp
@@ -1,1 +1,13 @@
-Sum
+//	1742A - Sum
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t,a,b,c;
+	cin>>t;
+	while(t--){
+		cin>>a>>b>>c;
+		cout<<(a+b==c||b+c==a||c+a==b?"yes\n":"no\n");
+	}
+	
+}
