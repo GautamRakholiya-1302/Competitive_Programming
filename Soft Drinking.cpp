e52677008ef240ebccb5bb1c1960e8d21a7474fa
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-11-08 23:07:07 +0530

    Update Soft Drinking.cpp

--- Soft Drinking.cpp
+++ Soft Drinking.cpp
@@ -1,1 +1,21 @@
-
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int n, k, l, c, d, p, nl, np;
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+ 
+    int total_drink = k * l;
+    int total_limes = c * d;
+    int total_salt = p;
+ 
+    int toasts_drink = total_drink / (n * nl);
+    int toasts_limes = total_limes / n;
+    int toasts_salt = total_salt / (n * np);
+ 
+    int min_toasts = min(min(toasts_drink, toasts_limes), toasts_salt);
+    cout << min_toasts << endl;
+ 
+    return 0;
+}
