6b6267968cdf9413c4d49c3d29c51c6d34cfb2da
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-11-28 23:12:16 +0530

    Create Dislike of Threes.cpp

---
+++ Dislike of Threes.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+int t;
+cin >> t;
+while (t--)
+{
+	int k;
+	cin >> k;
+	int j=0,res=0,a=0;
+while (j<k)
+{
+	if(a%3!=0 && a%10!=3)
+	{
+		res=a;
+		j++;
+	}
+	a++;
+}
+cout <<res << endl;
+	
+}
+return 0;
+}
