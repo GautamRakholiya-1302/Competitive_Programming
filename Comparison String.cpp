1ab392952b5d2681b1c8ed1cc0e3f95bc9c559bd
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-12-16 23:32:56 +0530

    Update Comparison String.cpp

--- Comparison String.cpp
+++ Comparison String.cpp
@@ -1,1 +1,19 @@
 //1837B - Comparison String
+#include <bits/stdc++.h>
+using namespace std;
+int a[110], t[10010];
+void solve() {
+	  int n, cnt = 1, ans = 1; string s;
+	  cin >> n >> s;
+	  for(int i = 1; i < n; i++) {
+		   if(s[i] == s[i - 1]) ++cnt;
+		   else cnt = 1;
+		   ans = max(ans, cnt);
+	  }
+	  cout << ans + 1 << endl;
+}
+ 
+int main() {
+	int T; cin >> T;
+	while(T--) solve();
+}
