62ed06baf6da7701c9b84fc4264c2d31b76d77bf
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-12-16 23:28:01 +0530

    Update Vasilije in Cacak.cpp

--- Vasilije in Cacak.cpp
+++ Vasilije in Cacak.cpp
@@ -1,1 +1,14 @@
-1878C - Vasilije in Cacak
+//1878C - Vasilije in Cacak
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int t;
+    cin >> t;
+    while (t--)
+    {
+        long long int n, k, x;
+        cin >> n >> k >> x;
+        cout << ((k * (k + 1) <= 2 * x && 2*x <= n * (n + 1) - (n - k) * (n - k + 1)) ? "YES" : "NO") << endl;
+    }
+}
