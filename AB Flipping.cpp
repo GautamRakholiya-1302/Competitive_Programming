1fdbbf16847de77a5c9a8b25e1bfb4fadd4de4ba
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-12-16 23:26:06 +0530

    Update AB Flipping.cpp

--- AB Flipping.cpp
+++ AB Flipping.cpp
@@ -1,1 +1,23 @@
-/
+//1896B - AB Flipping
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		string s;
+		cin>>s;
+		int k=0;
+		int h=s.size()-1;
+		while(s[k]=='B') k++;
+		while(s[h]=='A') h--;
+		if(h-k<0){
+			cout<<0<<endl;
+			continue;
+		}
+		cout<<h-k<<endl;
+	}
+}
