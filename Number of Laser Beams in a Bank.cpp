3b5336dba0677058fb94e13f7208c026c5646401
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-01-03 22:26:58 +0530

    Update Number of Laser Beams in a Bank.cpp

--- Number of Laser Beams in a Bank.cpp
+++ Number of Laser Beams in a Bank.cpp
@@ -1,1 +1,30 @@
-Number of Laser Beams in a Bank
+
+//Number of Laser Beams in a Bank
+
+
+class Solution {
+public:
+    int numberOfBeams(std::vector<std::string>& bank) {
+        int prevRowCount = 0;
+        int total = 0;
+
+        for (const std::string& row : bank) {
+            int curRowCount = calc(row);
+            if (curRowCount == 0)
+                continue;
+
+            total += curRowCount * prevRowCount;
+            prevRowCount = curRowCount;
+        }
+        return total;
+    }
+
+private:
+    int calc(const std::string& s) {
+        int count = 0;
+        for (char c : s)
+            count += c - '0';
+
+        return count;
+    }
+};
