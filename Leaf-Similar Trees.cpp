92b6b68b57df9431ad963de73371e0e8c89d2424
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-01-09 22:09:46 +0530

    Update Leaf-Similar Trees.cpp

--- Leaf-Similar Trees.cpp
+++ Leaf-Similar Trees.cpp
@@ -1,1 +1,29 @@
 Leaf-Similar Trees
+class Solution {
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        string t1;
+        string t2;
+
+        dfs(root1,t1);
+        dfs(root2,t2);
+        return t1==t2;
+
+
+    }
+
+    void dfs(TreeNode *root,string &s)
+    {
+        if(root==NULL)
+        {
+            return ;
+        }
+        if(root->left==NULL && root->right==NULL)
+        {
+            s+=to_string(root->val)+"#";
+        }
+        dfs(root->left,s);
+        dfs(root->right,s);
+
+    }
+};
