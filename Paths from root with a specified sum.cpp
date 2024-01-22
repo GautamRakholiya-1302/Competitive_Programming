9aa384abf438f6b5af49cb1c999a228c5b28ccb6
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-01-22 22:30:39 +0530

    Update Paths from root with a specified sum.cpp

--- Paths from root with a specified sum.cpp
+++ Paths from root with a specified sum.cpp
@@ -1,1 +1,20 @@
-
+class Solution
+{
+    public:
+     void help(Node *root,int s,int sum,vector<int> temp,vector<vector<int>> &ans){
+        if(!root)return;
+        s+=root->key;
+        temp.push_back(root->key);
+        if(s==sum)ans.push_back(temp);
+        help(root->left,s,sum,temp,ans);
+        help(root->right,s,sum,temp,ans);
+    }
+    vector<vector<int>> printPaths(Node *root, int sum)
+    {
+        //code here
+        vector<vector<int>> ans;
+        vector<int> temp;
+        help(root,0,sum,temp,ans);
+        return ans;
+    }
+};
