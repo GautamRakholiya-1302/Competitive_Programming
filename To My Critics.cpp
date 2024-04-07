f3e589b5189c4f903c3f4527e0811b88993a4756
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-04-07 17:59:48 +0530

    Update To My Critics.cpp

--- To My Critics.cpp
+++ To My Critics.cpp
@@ -1,1 +1,23 @@
 //1850A	To My Critics
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c;
+        cin>>a;
+        cin>>b;
+        cin>>c;
+        if(a+b>=10 || c+b>=10 || a+c>=10)
+        {
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+      }
+    }
+    
+}
